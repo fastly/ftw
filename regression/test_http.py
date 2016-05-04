4169f87bc17bed5ae8d8bc45cdc653334a1b5ab0
Chaim sanders <csanders@sparsa.org>
2016-05-03 22:48:21 -0400

    Added more test

--- regression/test_http.py
+++ regression/test_http.py
@@ -18,10 +18,25 @@ def main():
     # issue web request
     print http_ua.send_request()[0]
 
-    # Basic 
+    # Basic GET without Host on 1.1 - Expect 400 
     x = ruleset.Input(dest_addr="example.com",headers={})
     http_ua = http.HttpUA(x)
+    print http_ua.send_request()[0]
+    
+    # Basic GET without Host on 1.0 - Expect 404 (server is VHosted)
+    x = ruleset.Input(dest_addr="example.com",version="HTTP/1.0",headers={})
+    http_ua = http.HttpUA(x)
+    print http_ua.send_request()[0]
+    
+    # Basic GET wit Host on 1.0 - Expect 200
+    x = ruleset.Input(dest_addr="example.com",version="HTTP/1.0",headers={"Host":"example.com"})
+    http_ua = http.HttpUA(x)
+    print http_ua.send_request()[0]
 
-    # check response and assert
+    # Basic GET without Host on 0.9 - Expect 505 version not supported
+    x = ruleset.Input(dest_addr="example.com",version="HTTP/0.9",headers={})
+    http_ua = http.HttpUA(x)
+    print http_ua.send_request()[0]
+
 
 main()
