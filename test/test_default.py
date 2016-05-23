8348fa26b81fcd2a7230f01f7035c8b5da952346
Zack Allen <zallen@fastly.com>
2016-05-23 11:00:32 -0400

    Added a default tester for status code & html contains checks, no logger

---
+++ test/test_default.py
@@ -0,0 +1,17 @@
+import pytest
+from ftw import testrunner, errors
+
+def test_default(, ruleset, test, destaddr):
+    """
+    Default tester with no logger obj. Useful for HTML contains and Status code
+    Not useful for testing loggers
+    """
+    runner = testrunner.TestRunner() 
+    try:
+        for stage in test.stages:
+            if destaddr is not None:
+                stage.input.dest_addr = destaddr
+            runner.run_stage(stage, None)
+    except errors.TestError as e:
+        e.args[1]['meta'] = ruleset.meta
+        pytest.fail('Failure! Message -> {0}, Context -> {1}'
