0720b5e7f20c365924b9ea69ca2de9747a706e55
Zack Allen <zallen@fastly.com>
2016-05-23 16:17:21 -0400

    Added negative test for not finding a log

--- test/integration/test_logcontains.py
+++ test/integration/test_logcontains.py
@@ -4,8 +4,14 @@ import random
 import threading
 
 class LoggerTestObj(logchecker.LogChecker):
+    def __init__(self):
+        self.do_nothing = False
+
     def generate_random_logs(self):
-        return [str(self.start) + ' rule-id-' + str(random.randint(10,99))]
+        if self.do_nothing:
+            return []
+        else:
+            return [str(self.start) + ' rule-id-' + str(random.randint(10,99))]
         
     def get_logs(self):
         logs = self.generate_random_logs()
@@ -18,11 +24,22 @@ def logchecker_obj():
     """
     return LoggerTestObj()
 
-def test_logcontains_integration(logchecker_obj, ruleset, test, http_serv_obj):
+def test_logcontains_withlog(logchecker_obj, ruleset, test, http_serv_obj):
     thread = threading.Thread(target = http_serv_obj.serve_forever)
     thread.daemon = True
     thread.start()
     runner = testrunner.TestRunner()
     for stage in test.stages:
         runner.run_stage(stage, logchecker_obj)
+    http_serv_obj.shutdown()
+
+def test_logcontains_nolog(logchecker_obj, ruleset, test, http_serv_obj):
+    logchecker_obj.do_nothing = True
+    thread = threading.Thread(target = http_serv_obj.serve_forever)
+    thread.daemon = True
+    thread.start()
+    runner = testrunner.TestRunner()
+    with(pytest.raises(AssertionError)):
+        for stage in test.stages:
+            runner.run_stage(stage, logchecker_obj)
     http_serv_obj.shutdown()
