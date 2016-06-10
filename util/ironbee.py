bf540f27822d953b01291217a9e0e462b117698b
Chaim sanders <csanders@sparsa.org>
2016-06-10 11:01:55 -0400

    Updating support for ironbee tests

--- util/ironbee.py
+++ util/ironbee.py
@@ -7,20 +7,72 @@ for root, dirs, files in os.walk("waf-research", topdown=False):
         extension = name[-5:]
         if extension == ".test":
             filelist.append(os.path.join(root, name))
+idcounter = 1000
+tests = {}
+
+
 for fname in filelist:
+    testName = fname.split('/')[-2]
+    if( testName not in tests.keys() ):
+        tests[testName] = request_to_yaml.Request()
+    # continously readd the title
+    tests[testName].add_fname(testName+'.yaml')
+    # incriment each test ID
+    tests[testName].set_id(idcounter)
+    idcounter += 1     
     f = open(fname,'r')
     request = ""
     for line in f.readlines():
         if line[0] != '#':
             request += line
-    req = request_to_yaml.Request()
-    request = request.replace('\n','\r\n')
-    req.get_request_line(request)
-    req.get_headers(request)
-    req.get_data(request)
-    yaml_out = req.generate_yaml()
-    newfname =  (fname.split('/')[-1]).split('.')[0]
-    print newfname
-    req.write_yaml('output/'+newfname+'.yaml',yaml_out)
-    #print yaml_out
+        else:
+            # ignore empty lines
+            if line.strip() == "#":
+                pass
+            else:
+                if line.find("# @") != -1:
+                    pass
+    request = request.replace('\n','\r\n')                    
+    tests[testName].get_request_line(request)
+    tests[testName].get_headers(request)
+    tests[testName].get_data(request)    
+    tests[testName].generate_test()
+
+# Generate our test
+for test in tests.keys():
+    yaml_out = "test"
+    tests[test].generate_header()
+#    req.generate_test()
+    yaml_out = tests[test].generate_yaml()    
+    tests[test].write_yaml('output/'+test+'.yaml',yaml_out)
+#    tests[test].write_yaml('output/'+test+'.yaml',yaml_out)
+
+#    f = open(fname,'r')
+#    request = ""
+#    req = request_to_yaml.Request()
+#    for line in f.readlines():
+#        if line[0] != '#':
+#            request += line
+#        else:
+#            # ignore empty lines
+#            if line.strip() == "#":
+#                pass
+#            else:
+#                if line.find("# @") != -1:
+#                    pass
+#                    #print line
+#                else:
+#                    req.add_description(line[2:].strip())
+#    req.set_id(idcounter)
+#    idcounter += 1                  
+#    request = request.replace('\n','\r\n')
+#    req.get_request_line(request)
+#    req.get_headers(request)
+#    req.get_data(request)
+#    newfname =  (fname.split('/')[-1]).split('.')[0]
+#    req.add_fname(newfname+'.yaml')
+#    req.generate_header()
+#    req.generate_test()
+#    yaml_out = req.generate_yaml()
+#    req.write_yaml('output/'+newfname+'.yaml',yaml_out)
 
--- util/request_to_yaml.py
+++ util/request_to_yaml.py
@@ -5,30 +5,44 @@ class Request():
         self.input = {}
         self.output = {}
         self.data_start = 0
+        self.description = ""
+        self.fname = ""
+        self.id = -1
+        self.data = {"meta":{},"tests":[]}
     def double_quote(self,mystr):
         return mystr
         return "\"" + str(mystr) + "\""
-    def generate_yaml(self):
-        data = dict(
-            meta = dict(
-                author = "Zack",
+    def generate_header(self):
+        self.data["meta"] = dict(
+                author = "@csanders-git",
                 enabled = True,
-                name = "EXAMPLE.yaml",
-                description = "Description"
-            ),
-            tests = [dict(
-                rule_id = 1234,
-                stages = [dict(
-                    stage = dict(
-                        input = self.input,
-                        output = dict(
-                            status = 200
-                        )
+                name = self.fname,
+                description = self.description 
+            )
+           
+    def generate_yaml(self):
+        #print self.data        
+        return yaml.dump(self.data, default_flow_style=False)
+    def generate_test(self):
+        print   self.input
+
+        self.data["tests"].append(dict(
+            rule_id = self.id,
+            stages = [dict(
+                stage = dict(
+                    input = self.input,
+                    output = dict(
+                        status = 200
                     )
-                )]
+                )
             )]
-        )
-        return yaml.dump(data, default_flow_style=False)
+        ))
+        self.input = {}
+        #print self.data
+    def add_description(self, description):
+        self.description = description
+    def add_fname(self, name):
+        self.fname = name     
     def get_request_line(self, req):
         req = req.split('\r\n')[0]
         req = req.split(' ',2)
@@ -49,7 +63,8 @@ class Request():
     def get_data(self, req):
         req = req.split('\r\n')[1:]
         self.input["data"] = self.double_quote("\r\n".join(req[self.data_start+1:]))
-
+    def set_id(self, id):
+        self.id = id
     def write_yaml(self,fname, yaml_out):
         f = open(fname,'w')
         f.write(yaml_out)
