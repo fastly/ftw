ae066045a2fad20f6b57cbd0f91a82bdbd1b897a
csanders-git <csanders@sparsa.org>
2016-07-21 12:27:47 -0400

    fixing missing comma

--- ftw/http.py
+++ ftw/http.py
@@ -193,7 +193,7 @@ class HttpResponse(object):
                 'The status num of the response line isn\'t convertable',
                 {
                     'msg': 'This may be an HTTP 1.0 \'Simple Req\\Res\', it \
-                    doesn\'t have HTTP headers and FTW will not parse these'
+                    doesn\'t have HTTP headers and FTW will not parse these',
                     'response_line': str(response_line),
                     'function': 'http.HttpResponse.process_response'
                 })
